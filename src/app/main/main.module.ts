08324b4b4c8e295bbffc4db1add420db7d3eb83f
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2021-01-31 22:30:37 -0600

    :art: register

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MaterialModule } from '../material/material.module';
 
-import { PostulationComponent } from './pages/postulation/postulation.component';
-import { RegisterComponent } from './pages/register/register.component';
-
-
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { MainComponent } from './main.component';
 import { FormEducationComponent } from './components/form-education/form-education.component';
 import { FormLanguagesComponent } from './components/form-languages/form-languages.component';
+import { PostulationComponent } from './pages/postulation/postulation.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
