571dd3030e6565880c03492b380d4a0cfb4945e8
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2021-02-01 21:24:33 -0600

    :add: firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+  firebaseConfig: {
+    apiKey: 'AIzaSyA9MmL1uMnCrwxSzyt85nFhKKcWbL5a_Xo',
+    authDomain: 'register-postulation-7a859.firebaseapp.com',
+    projectId: 'register-postulation-7a859',
+    storageBucket: 'register-postulation-7a859.appspot.com',
+    messagingSenderId: '297477873674',
+    appId: '1:297477873674:web:d9ab49ab82743be3d53ff3'
   }
 };
 
